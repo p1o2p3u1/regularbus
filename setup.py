cafc1758238560d42de70bf16fbd239336e2a519
ting.wu <ting.wu@corp.netease.com>
2015-07-22 18:14:21 +0800

    update config

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='regularbus',
+    packages=['regularbus'],
+    version='0.1',
+    description='python coverage test tool',
+    author='ting wu',
+    author_email='ting.wu@corp.netease.com',
+    url='',
+    download_url='',
+    keywords=['testing', 'coverage'],
+    classifiers=[],
+)
